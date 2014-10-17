18ba047cde5c6d1d924df3786f45975d4ec4dd2d
hectorescalante <hesaesal@gmail.com>
2014-10-17 00:06:32 -0500

    Cambios Hector 2

--- app/src/main/java/com/example/first/test1/MiActividad.java
+++ app/src/main/java/com/example/first/test1/MiActividad.java
@@ -14,7 +14,6 @@ public class MiActividad extends Activity {
         setContentView(R.layout.activity_mi_actividad);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
